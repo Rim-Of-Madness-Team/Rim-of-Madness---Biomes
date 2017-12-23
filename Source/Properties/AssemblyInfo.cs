fd98bca4bfe7533a0e5f739afcf6c98bd144d2ab
jecrell <matt.walls31@gmail.com>
2017-12-23 11:35:00 +0900

    Initial commit

---
+++ Source/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RoMBiomes")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RoMBiomesDLL")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4bb3702a-d5df-4c61-806e-ca84508d5b4d")]
+
+[assembly: AssemblyVersion("1.18.0.0")]
+[assembly: AssemblyFileVersion("1.18.0.0")]
---
+++ Source/RoMBiomes/BiomeWorker_HauntedForest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using RimWorld.Planet;
+using Verse;
+
+namespace RoMBiomes
+{
+    public class BiomeWorker_HauntedForest : BiomeWorker_TemperateForest
+    {
+        public override float GetScore(Tile tile)
+        {
+            if (tile.WaterCovered)
+            {
+                return -100f;
+            }
+            if (tile.temperature < -10.0)
+            {
+                return 0f;
+            }
+            if (tile.rainfall < 700.0)
+            {
+                return 0f;
+            }
+            //if (Rand.Value > .008)
+            //{
+            //    return 0f;
+            //}
+            return 100000f;
+            //return (float)(15.0 + (tile.temperature - 5.0) + (tile.rainfall - 600.0) / 180.0);
+        }
+    }
+}
